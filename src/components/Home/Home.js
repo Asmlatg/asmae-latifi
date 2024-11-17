2e9ca5c050b4f1714679de3dab36de8e65a7381a
Asmlatg <asmaelatifi63@gmail.com>
2024-11-17 21:16:13 +0100

    change to blue <3

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -22,7 +21,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Asmae LATIFI</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -30,13 +29,7 @@ function Home() {
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
+            <Col md={5} style={{ paddingBottom: 20, backgroundColor: "#0a192f" }}>
             </Col>
           </Row>
         </Container>
