ccd63eb57a063246d941ab2778665da8618a34bd
Asmlatg <asmaelatifi63@gmail.com>
2024-12-12 21:17:03 +0100

    erty

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/Asmlatg"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -27,7 +27,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/asmae-latifi-873b19292/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
